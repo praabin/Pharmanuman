f53ae4d674afb1b681f5e4a589d24c8d2f54ceb3
prabin.sc1 <prabin.sc1@gmail.com>
2023-06-06 20:08:03 +0545

    initial commit

---
+++ Pharmanuman/src/main/java/com/pharmanuman/PharmanumanApplication.java
@@ -0,0 +1,14 @@
+package com.pharmanuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmanumanApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Surawat vagelai");
+		SpringApplication.run(PharmanumanApplication.class, args);
+	}
+
+}
