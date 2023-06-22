18511fa9dbc7f8df610fbcf354fc164b1b2dba67
prabin.sc1 <prabin.sc1@gmail.com>
2023-06-22 11:01:10 +0545

    password change module implemented

--- Pharmanuman/src/main/java/com/pharmanuman/controller/PharmacyController.java
+++ Pharmanuman/src/main/java/com/pharmanuman/controller/PharmacyController.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pharmanuman.dao.MedicineRepository;
 import com.pharmanuman.dao.UserRepository;
@@ -25,6 +27,9 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/pharmacy")
 public class PharmacyController {
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
 	private UserRepository userRepository;
@@ -53,12 +58,6 @@ public class PharmacyController {
 		return "pharmacy/profile";
 	}
 
-	@RequestMapping("/setting")
-	public String setting(Model model) {
-		model.addAttribute("title", "User Dashboard");
-		return "pharmacy/setting";
-	}
-
 	@GetMapping("/order-medicine")
 	public String openAddContactForm(Model model, Principal p) {
 		model.addAttribute("title", "Order medicine");
@@ -74,29 +73,29 @@ public class PharmacyController {
 	public String processOrder(@ModelAttribute Medicine medicine, Principal p, Model m, HttpSession session) {
 
 		try {
-		String tempName = p.getName();
-		User user = this.userRepository.getUserByUserName(tempName);
-		medicine.setUser(user);
-		user.getMedicines().add(medicine);
-		this.userRepository.save(user);
+			String tempName = p.getName();
+			User user = this.userRepository.getUserByUserName(tempName);
+			medicine.setUser(user);
+			user.getMedicines().add(medicine);
+			this.userRepository.save(user);
 
-		System.out.println("data: " + medicine);
-		System.out.println("medicines added to database");
-		String name = p.getName();
-		User tempUser = this.userRepository.getUserByUserName(name);
-		List<Medicine> medicines = this.medicineRepository.findMedicinesById(tempUser.getId());
-		Collections.sort(medicines, Comparator.comparingInt(Medicine::getMid).reversed());
-		m.addAttribute("medicines", medicines);
-		session.setAttribute("msg", new MyMessage("Successfully added!! ","alert-success"));
-		return "pharmacy/add_medicine";
-		
-		}catch (Exception e) {
+			System.out.println("data: " + medicine);
+			System.out.println("medicines added to database");
+			String name = p.getName();
+			User tempUser = this.userRepository.getUserByUserName(name);
+			List<Medicine> medicines = this.medicineRepository.findMedicinesById(tempUser.getId());
+			Collections.sort(medicines, Comparator.comparingInt(Medicine::getMid).reversed());
+			m.addAttribute("medicines", medicines);
+			session.setAttribute("msg", new MyMessage("Successfully added!! ", "alert-success"));
+			return "pharmacy/add_medicine";
+
+		} catch (Exception e) {
 			// TODO: handle exception
 			m.addAttribute("medicines", medicine);
-			session.setAttribute("msg", new MyMessage("Something went wrong!! "+e.getMessage(),"alert-danger"));
+			session.setAttribute("msg", new MyMessage("Something went wrong!! " + e.getMessage(), "alert-danger"));
 			return "pharmacy/add_medicine";
 		}
-		
+
 	}
 
 	/*
@@ -180,6 +179,40 @@ public class PharmacyController {
 		 * model.addAttribute("medicines", medicines);
 		 */
 		return "pharmacy/update_medicine";
+	}
+
+	// password change module
+	@RequestMapping("/setting")
+	public String setting(Model model) {
+		model.addAttribute("title", "User Dashboard");
+		return "pharmacy/setting";
+	}
+
+	@PostMapping("/change-password")
+	public String changePassword(@RequestParam("oldPassword") String oldPassword,
+			@RequestParam("newPassword") String newPassword, Principal p, HttpSession session) {
+		System.out.println("Old password::" + oldPassword);
+		String name = p.getName();
+
+		User tempUser = this.userRepository.getUserByUserName(name);
+		String oldPassword1 = tempUser.getPassword();
+
+		if (this.bCryptPasswordEncoder.matches(oldPassword, oldPassword1)) {
+			tempUser.setPassword(this.bCryptPasswordEncoder.encode(newPassword));
+			this.userRepository.save(tempUser);
+
+			this.userRepository.save(tempUser);
+			session.setAttribute("msg", new MyMessage("Successfully Updated!! ", "alert-success"));
+			return "pharmacy/setting";
+		} else {
+			System.out.println("password doesn't matches");
+			session.setAttribute("msg", new MyMessage("Password doesn't match. ", "alert-danger"));
+			return "pharmacy/setting";
+		
+		} 
+//		System.out.println("New password::" + newPassword);
+
+//		return "redirect:/pharmacy/index";
 	}
 
 }
