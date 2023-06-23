1eb72a83ce2c80a5ca854e1eea662f230d3dc4ca
prabin.sc1 <prabin.sc1@gmail.com>
2023-06-24 00:38:07 +0545

    Password change with otp done successfully

--- Pharmanuman/src/main/java/com/pharmanuman/controller/ForgotController.java
+++ Pharmanuman/src/main/java/com/pharmanuman/controller/ForgotController.java
@@ -1,15 +1,33 @@
 package com.pharmanuman.controller;
 
+import java.security.Principal;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.pharmanuman.dao.UserRepository;
+import com.pharmanuman.entities.User;
+import com.pharmanuman.helper.MyMessage;
+import com.pharmanuman.services.EmailService;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class ForgotController {
-	
+
+	@Autowired
+	private EmailService emailService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	private Random r = new Random(100000);
 
@@ -19,7 +37,7 @@ public class ForgotController {
 	}
 
 	@PostMapping("/send-otp")
-	public String sendOTP(@RequestParam("email") String email) {
+	public String sendOTP(@RequestParam("email") String email, HttpSession session) {
 		System.out.println("Email: " + email);
 		// generating otp
 
@@ -27,8 +45,74 @@ public class ForgotController {
 
 		System.out.println("OTP:: " + otp);
 		// now it's time to write code to send otp to email
-		         
-		return "verify_otp";
+
+		String subject = "OTP from Pharmanuman";
+//		String message = "<h1> OTP = " + otp + "</h1>";
+
+		String message = "<div style=\"border: 1px solid #e1e1e1; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\">"
+				+ "<div style=\"background-color: #003399; padding: 20px; border-radius: 5px 5px 0 0;\">"
+				+ "<h1 style=\"color: #fff; margin: 0; font-size: 24px;\">Pharmanuman</h1>"
+				+ "<h3 style=\"color: #fff; margin-top: 10px; font-size: 18px;\">OTP Verification</h3>" + "</div>"
+				+ "<div style=\"padding: 20px;\">" + "<p style=\"font-size: 16px; color: #333;\">Dear User,</p>"
+				+ "<p style=\"font-size: 18px; color: #333;\">Your One-Time Password (OTP) for verification is:</p>"
+				+ "<h2 style=\"font-size: 28px; color: #333; margin-top: 10px;\">" + otp + "</h2>"
+				+ "<p style=\"font-size: 16px; color: #333;\">Please use this OTP to complete the verification process.</p>"
+				+ "</div>" + "<div style=\"background-color: #f8f8f8; padding: 20px; border-radius: 0 0 5px 5px;\">"
+				+ "<p style=\"font-size: 14px; color: #666;\">Pharmanuman - Secure Verification Service</p>" + "</div>"
+				+ "</div>";
+
+		String to = email;
+
+		boolean flag = this.emailService.sendTo(to, subject, message);
+
+		if (flag) {
+//			session.setAttribute("message", "Check your email id...");
+//			return "pharmacy/forgot_password";
+			session.setAttribute("myotp", otp);
+			session.setAttribute("email", email);
+			return "verify_otp";
+		} else {
+//			session.setAttribute("message", "OTP Sent Successfully...");
+			session.setAttribute("message", "Check your email id...");
+			return "pharmacy/forgot_password";
+
+		}
+
+	}
+
+	@PostMapping("/verify-otp")
+	public String verifyOTP(@RequestParam("otp") int otp, HttpSession session) {
+		int myOtp = (int) session.getAttribute("myotp");
+		String email = (String) session.getAttribute("email");
+
+		if (myOtp == otp) {
+			User tempUser = this.userRepository.getUserByUserName(email);
+
+			if (tempUser == null) {
+
+				session.setAttribute("message", "User doesn't exist with this email...");
+				return "pharmacy/forgot_password";
+
+			} else {
+
+				return "change_password";
+			}
+		} else {
+			session.setAttribute("message", "You have entered wrong OTP");
+			return "verify_otp";
+		}
+
+	}
+
+	@PostMapping("/change-password-form")
+	public String changePassword(@RequestParam("newPassword") String newPassword, HttpSession session) {
+		String email = (String) session.getAttribute("email");
+
+		User tempUser = this.userRepository.getUserByUserName(email);
+		tempUser.setPassword(this.bCryptPasswordEncoder.encode(newPassword));
+		this.userRepository.save(tempUser);
+
+		return "redirect:/signin?change=Password change successfully";
 	}
 
 }
---
+++ Pharmanuman/src/main/java/com/pharmanuman/services/EmailService.java
@@ -0,0 +1,72 @@
+package com.pharmanuman.services;
+
+import java.util.Properties;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+	
+	public boolean sendTo(String to, String subject, String message) {
+
+		boolean f = false;
+
+		String from = "prabinshah2018@gmail.com";
+
+		String host = "smtp.gmail.com";
+
+		Properties properties = System.getProperties();
+		System.out.println("Properties:: " + properties);
+
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// Step 1: to get session object
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("prabinshah2018@gmail.com", "sntqbblhdlepxyls");
+			}
+
+		});
+
+		session.setDebug(true);
+
+		// step 2: compose the message text, multimedia
+
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+			mimeMessage.setFrom(from);
+			mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			mimeMessage.setSubject(subject);
+//			mimeMessage.setText(message);
+			mimeMessage.setContent(message,"text/html");
+			
+			
+
+			// step 3 send message using Transport class
+			Transport.send(mimeMessage);
+			System.out.println("Sent success....");
+			f = true;
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		return f;
+
+	}
+
+
+}
